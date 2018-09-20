fa5c8264a35f780854b2c21757daa6576e8228b5
Jordan C <jordanpcasel@gmail.com>
2018-09-19 21:59:39 -0700

    all files pushed

--- static/js/beer.js
+++ static/js/beer.js
@@ -1,17 +1,10 @@
 console.log("Hello");
-var myMap = L.map("map", {
-  center: [0, -0],
-  zoom: 3
-});
+// var myMap = L.map("map", {
+//   center: [0, -0],
+//   zoom: 3
+// });
 
 const API_KEY = "pk.eyJ1IjoiZnZ1OTU4IiwiYSI6ImNqbTVldHhwajBlZXgzcXJ0bno3bDloaXkifQ.aMSQpbW9A8g7YU9n2If_bg"
-
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
 
 var geojsonPath = "static/js/vectormap.json";
 
@@ -34,16 +27,76 @@ d3.json(geojsonPath, function (geojsonData) {
         }
       });
     });
-
-    var beer = L.choropleth(geojsonData, {
-      valueProperty: "Beer_Percent",
-      scale: ["white","red"],
-      weight: 0.25,
-      color: "black",
-      opacity: 100,
-      fillOpacity: .75,
-      steps: 100
-
-    }).addTo(myMap);
   });
 });
+
+function createMap(alcohol) {
+  var mapbox = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  var baseMaps = {
+    "Tile Map": mapbox
+  };
+
+  var overlayMaps = {
+    Beer: beerMap,
+    Wine: wineMap,
+    Spirits: spiritsMap,
+    Other: otherMap
+  };
+
+  var myMap = L.map("map", {
+    center: [0, -0],
+    zoom: 3,
+    layers: [mapbox, beerMap, wineMap, spiritMap, otherMap]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+};
+
+
+// var beerMap = L.choropleth(geojsonData, {
+//   valueProperty: "Beer_Percent",
+//   scale: ["white", "red"],
+//   weight: 0.25,
+//   color: "black",
+//   opacity: 100,
+//   fillOpacity: .75,
+//   steps: 100
+// });
+
+// var wineMap = L.choropleth(geojsonData, {
+//   valueProperty: "Wine_Percent",
+//   scale: ["white", "purple"],
+//   weight: 0.25,
+//   color: "black",
+//   opacity: 100,
+//   fillOpacity: .75,
+//   steps: 100
+// });
+
+// var spiritsMap = L.choropleth(geojsonData, {
+//   valueProperty: "Spirits_Percent",
+//   scale: ["white", "orange"],
+//   weight: 0.25,
+//   color: "black",
+//   opacity: 100,
+//   fillOpacity: 1,
+//   steps: 100
+// });
+
+// var otherMap = L.choropleth(geojsonData, {
+//   valueProperty: "Other_Percent",
+//   scale: ["white", "brown"],
+//   weight: 0.25,
+//   color: "black",
+//   opacity: 100,
+//   fillOpacity: 1,
+//   steps: 100
+// });
